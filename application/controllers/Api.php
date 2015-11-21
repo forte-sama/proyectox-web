6791dde99ffea3a31c64903158a2a60f9606fb52
saleta15 <saletamanuel@gmail.com>
2015-11-20 22:38:15 -0400

    FIX: Ahora el API recibe peticiones HTTP en formato JSON
    Small fix, descomentar la linea para registrar usuario

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -3,43 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends CI_Controller {
     public function request_registro(){
-        $data = array(
 
-            'username' => $this->input->post('username'),
-            'password' => $this->input->post('password'),
-            'telefono' => $this->input->post('telefono'),
-            'email' => $this->input->post('email'),
-            'nombre' => $this->input->post('nombre'),
-            'apellido' => $this->input->post('apellido'),
-            'sexo' => $this->input->post('sexo'),
-            'fecha_nacimiento' => $this->input->post('fecha_nacimiento'),
-            'tipo_sangre' => $this->input->post('tipo_sangre'));
-            //  $data = array(
-            //
-            //     'username' => 'sasulo',
-            //     'password' => 'asdfasdf',
-            //     'telefono' =>'564-445-4555',
-            //     'email' => 'email@mmg.com',
-            //     'nombre' => 'nombre',
-            //     'apellido' => 'apellido',
-            //     'sexo' => 'M',
-            //     'fecha_nacimiento' => '11-10-2003',
-            //     'tipo_sangre' => 'O+');
+          $input_data = json_decode(trim(file_get_contents('php://input')),true);
 
-            echo $data['username'].$data['password'].$data['telefono'].$data['email'].$data['nombre'].$data['apellido'].$data['sexo'].$data['fecha_nacimiento'].$data['tipo_sangre'];
+          echo $input_data['username'].$input_data['password'].$input_data['telefono'].$input_data['email'].$input_data['nombre'].$input_data['apellido'].$input_data['sexo'].$input_data['fecha_nacimiento'].$input_data['tipo_sangre'];
 
-            $this->load->model("Usuario_movil");
-            $nuevo_usuario= new Usuario_movil();
-            $nuevo_usuario->username=$data['username'];
-            $nuevo_usuario->password=$data['password'];
-            $nuevo_usuario->telefono=$data['telefono'];
-            $nuevo_usuario->email=$data['email'];
-            $nuevo_usuario->nombre=$data['nombre'];
-            $nuevo_usuario->apellido=$data['apellido'];
-            $nuevo_usuario->sexo=$data['sexo'];
-            $nuevo_usuario->fecha_nacimiento=$data['fecha_nacimiento'];
-            $nuevo_usuario->tipo_sangre=$data['tipo_sangre'];
-            $nuevo_usuario->save();
+          $this->load->model("Usuario_movil");
+          $nuevo_usuario= new Usuario_movil();
+          $nuevo_usuario->username=$input_data['username'];
+          $nuevo_usuario->password=$input_data['password'];
+          $nuevo_usuario->telefono=$input_data['telefono'];
+          $nuevo_usuario->email=$input_data['email'];
+          $nuevo_usuario->nombre=$input_data['nombre'];
+          $nuevo_usuario->apellido=$input_data['apellido'];
+          $nuevo_usuario->sexo=$input_data['sexo'];
+          $nuevo_usuario->fecha_nacimiento=$input_data['fecha_nacimiento'];
+          $nuevo_usuario->tipo_sangre=$input_data['tipo_sangre'];
+
+
+          $nuevo_usuario->save();
     }
 
     public function request_edicion(){
@@ -80,7 +62,8 @@ class Api extends CI_Controller {
             $nuevo_usuario->sexo=$data['sexo'];
             $nuevo_usuario->fecha_nacimiento=$data['fecha_nacimiento'];
             $nuevo_usuario->tipo_sangre=$data['tipo_sangre'];
-            $nuevo_usuario->save();
+            //$nuevo_usuario->save();
+
     }
 
 }
