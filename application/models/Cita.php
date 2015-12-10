13035dca31d198a2b60c5e1f942c251b57c6b914
Cesar <mrcesar9402@gmail.com>
2015-12-09 23:59:13 -0400

    T#93 Vista de registro general

--- application/models/Cita.php
+++ application/models/Cita.php
@@ -11,35 +11,6 @@ class Cita extends MY_Model {
     public $cliente_presente;
     public $doctor;
     public $usuario_movil;
-    public $telefono;
-    public $estado_cita;
-
-    public function get($limit=0,$offset=0,$parent_get=TRUE){
-        if ($parent_get){
-            return parent::get($limit,$offset);
-        }
-        else{
-            $ret_val = array();
-            $query = $this->db->get_where($this::DB_TABLE, array(
-                "fecha" => $this->fecha,
-                "doctor"=> $this->doctor
-            ));
-            return $query->result();
-        }
-    }
-<?php
-class Cita extends MY_Model {
-
-    const DB_TABLE = 'cita';
-    const DB_TABLE_PK = 'cod_cita';
-    const DB_LAST_ID_SEQ = 'cita_cod_cita_seq';
-
-    public $cod_cita;
-    public $hora_programada;
-    public $fecha;
-    public $cliente_presente;
-    public $doctor;
-    public $usuario_movil;
     // public $telefono;
     //------------------
     public $nombre;
@@ -85,8 +56,6 @@ class Cita extends MY_Model {
     public function mostrar_fecha() {
         return date('M j, Y', strtotime($this->fecha));
     }
-
-}
 
     public function display_hour() {
         $ar = explode(":",$this->hora_programada);
--- application/views/registro_general.php
+++ application/views/registro_general.php
@@ -4,10 +4,44 @@
 <!-- /. NAV SIDE  -->
 <div id="page-wrapper" >
     <div id="page-inner">
-        <div class="jumbotron">
-            <h1>Baron! Esto es una pagina en blanco</h1>
-            <p>Contiene un Jumbotron de Bootstrap como prueba</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Boton prueba</a></p>
+        <div class="row">
+            <div class="col col-md-12">
+                <h1>Deseo registrame como un...</h1>
+            </div>
+        </div>
+        <div class="row">
+        </div>
+        <div class="row">
+            <div class="col col-md-6">
+                <div class="well">
+                    <div class="text-center clear">
+                        <h1>
+                            <span class="fa-stack fa-2x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-stethoscope fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </h1>
+                        <h1>Medico</h1>
+                    </div>
+                    <p>Podre ver estadisticas de consultas realizadas, ver citas en el calendario y ver cola en mi consultorio, y mucho mas</p>
+                    <p><a class="btn btn-primary btn-lg" href="<?= base_url('registro/doctor/'); ?>" role="button">Registrarme como Medico</a></p>
+                </div>
+            </div>
+            <div class="col col-md-6">
+                <div class="well media">
+                    <div class="text-center clear">
+                        <h1>
+                            <span class="fa-stack fa-2x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-book fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </h1>
+                        <h1>Asistente</h1>
+                    </div>
+                    <p>Podre manejar estado de la cola en un consultorio, crear citas para su medico, y mucho mas</p>
+                    <p><a class="btn btn-primary btn-lg" href="<?= base_url('registro/asistente/'); ?>" role="button">Registrarme como Asistente</a></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
